13ce25facb309597bffd3eb2bace488f5d1fac1d
e-a-w- <eawatkins.0@gmail.com>
2020-08-14 17:20:33 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Article from "./components/articles/Article";
+import Articles from "./components/articles/Articles";
+import ArticleForm from "./components/articles/ArticleForm";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Container>
+        <Switch>
+          <Route path="/articles/new" component={ArticleForm} />
+          <Route path="/articles/:articleId/edit" component={ArticleForm} />
+          <Route path="/articles/:articleId" component={Article} />
+          <Route path="/articles" component={Articles} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/articles/ArticleForm.jsx
+++ src/components/articles/ArticleForm.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
 
 const ArticleForm = () => {
-  return <div></div>;
+  const initialState = { title: "", text: "" };
+  const [values, setValues] = useState(initialState);
+
+  const handleSubmit = (e) => {
+    // prevent HTML default submission
+    e.preventDefault();
+
+    // POST request to RESTful route (to be defined in backend)
+    fetch("/articles", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // let the user know that it went trough
+          alert("Article successfully created");
+          // when we get confirmation we can reset the form to its original state (empty)
+          setValues(initialState);
+        }
+      })
+      .catch((error) => alert(error));
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Title of your article..."
+          required={true}
+          onChange={(e) => setValues({ ...values, title: e.target.value })}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Text</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows="5"
+          placeholder="Text for your article..."
+          required={true}
+          onChange={(e) => setValues({ ...values, text: e.target.value })}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default ArticleForm;
